8cb7121ca73550489365f5494d63e774f02c7493
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2026-07-16 19:23:24 -0700

    html uuid update on crm dashboard

--- blueprints/crm_module.py
+++ blueprints/crm_module.py
@@ -72,9 +72,9 @@ def new_customer():
 # View Customer Details Route
 @crm_module_bp.route("/profile/<uuid>", methods=["GET"])
 @technician_required
-def customer_profile(customer_id):
+def customer_profile(uuid):
     customers = load_customers_file()
-    customer = next((c for c in customers if c["customer_id"] == customer_id), None)
+    customer = next((c for c in customers if c["uuid"] == uuid), None)
     if not customer:
         return render_template("errors/404.html"), 404
     return render_template("crm/customer_profile.html", customer=customer, loggedInTech=session["technician"])
